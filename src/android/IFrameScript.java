f7cfc4fb7c9f46a6aa619dab914e7d2b06acac1a
Eugene Cross <ecross@rocketlawyer.co.uk>
2021-04-16 16:00:09 +0100

    Return true in action check > tidy up unused code

--- src/android/IFrameScript.java
+++ src/android/IFrameScript.java
@@ -7,17 +7,7 @@ import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaInterface;
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
-
 import android.app.Activity;
-import android.view.View;
-import android.os.Bundle;
-;
-import android.webkit.WebChromeClient;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import com.rocketlawyer.mobile.android.R;
-
 import android.util.Log;
 
 public class IFrameScript extends CordovaPlugin {
@@ -25,64 +15,24 @@ public class IFrameScript extends CordovaPlugin {
 
     public void initialize(CordovaInterface cordova, CordovaWebViewImpl webView) {
         super.initialize(cordova, webView);
-
         Log.d(TAG, "Initializing IFrameScript");
-//        mWebView.setWeb
-//
-//        mWebView.getSettings().setJavaScriptEnabled(true);
-//        mWebView.setWebChromeClient(new WebChromeClient());
-//        mWebView.setWebViewClient(new WebViewClient());
-
-//        {
-//            @Override
-//            public void onPageFinished(WebView view, String url) {
-//                String user = ((EditText) findViewById(R.id.edit_text)).getText().toString();
-//                if (user.isEmpty()) {
-//                    user = "World";
-//                }
-//                String javascript="javascript: document.getElementById('msg').innerHTML='Hello "+user+"!';";
-//
-//
-//                view.loadUrl(url);
-//            }
-//        });
-
     }
 
     @Override
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
         Log.d(TAG, action);
-
-        if (action.equals("echo")) {
-            String phrase = args.getString(0);
-            this.echo(phrase, callbackContext);
-            return true;
-        }
-
         if (action.equals("addScriptCode")) {
             String scriptName = args.getString(0);
             String script = args.getString(1);
             Log.d(TAG, "about to load " + script);
-
             this.addScript(script, callbackContext);
-
-            
+            return true;
         }
-
         return false;
     }
 
-    private void echo(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            Log.d(TAG, message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument");
-        }
-
-    }
 
     private void addScript(String script, CallbackContext callbackContext) {
-
         CordovaWebView view = this.webView;
         Activity activity = this.cordova.getActivity();
         activity.runOnUiThread(new Runnable() {
@@ -90,13 +40,7 @@ public class IFrameScript extends CordovaPlugin {
             public void run() {
                 Log.d(TAG, "about to load on UI thread " + script + " into webview");
                 view.loadUrlIntoView(script, false);
-
             }
         });
-
-
-
     }
-
-
 }
